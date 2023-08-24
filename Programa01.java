ef5eb8242c7ae02b4e59f4ec292c9803ed39b0f8
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-08-24 14:12:20 -0300

    Variaveis

---
+++ Programa01.java
@@ -0,0 +1,21 @@
+//Variaveis
+
+public class Programa01{
+
+    public static void main(String[] args) {
+        //Declaração de variável
+        int valor;
+
+        //Declaração de variáveis
+        int num1, num2, num3;
+
+        //Declarando e inicializando algumas variáveis
+        int num4, num5 = 4, num6 = 7, num8; //Nesse casp, o num5 = 4 está declarando e inicializando
+
+        char caractere;
+        float preco;
+
+        float _valor = 34.2f; // Por padrão, os valores de ponto flutuante são double no Java
+        
+    }
+}
