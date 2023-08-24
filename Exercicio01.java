1b8021fcff0043bbc606d44db687d941932483d7
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-08-24 14:53:31 -0300

    Exercicio 1

---
+++ Exercicio01.java
@@ -0,0 +1,12 @@
+/*
+ * O objetivo nessa atividade é alterar o retorno da função
+ * minhaMensagem() para que ao inves de retornar 0,
+ * retorne outro texto.
+ */
+public class Exercicio01 {
+    public String mensagemCurso() {
+        return "Deu certo!";
+    }
+  }
+
+
