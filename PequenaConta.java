40ad8d6151ff7d65cd13748eec9ec58cab9383fb
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-08-24 15:04:28 -0300

    Pequeno teste de conta

---
+++ PequenaConta.java
@@ -0,0 +1,11 @@
+public class PequenaConta {
+        public static void main(String[] args) {
+            int num1 = 10;
+            int num2 = 20;
+            int resultado = num1 + num2;
+            System.out.println("O resultado é " + resultado);
+        }
+    
+    }
+
+   
