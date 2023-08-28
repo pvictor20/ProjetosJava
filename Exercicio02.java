8d4a8ddb18054fb524131725a96d940d312a2c4e
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-08-28 14:14:44 -0300

    Exercício 2

---
+++ Exercicio02.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exercicio02 {
+    public static void main(String[] args) {
+       
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Digite um número inteiro: ");
+        int numero = teclado.nextInt();
+        
+        System.out.println("Parabens o numero  " + numero + " é inteiro");
+
+       
+
+
+    }
+}
