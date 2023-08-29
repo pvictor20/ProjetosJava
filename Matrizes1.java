be14766663f446e7f35304f817864a2efb25d6d3
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-08-29 18:23:15 -0300

    Matrizes parte 1

---
+++ Matrizes1.java
@@ -0,0 +1,35 @@
+/*
+ * Vetores/arrays/matrizes multi dimensionais
+  //vetor
+ int numeros [3];
+numeros [0] = 3;
+numeros [1] = 2;
+numeros[2] = 5;
+
+//matriz
+[linhas][colunas]
+int numeros [3][3]
+
+numeros [0][0] = 1;
+numeros [0][1] = 2
+numeros [0][2] = 5;
+
+numeros [1][0] = 7;
+numeros [1][1] = 4;
+numeros [1][2] = 9;
+
+numeros [2][0] = 3;
+numeros [2][1] = 6;
+numeros [2][2] = 32;
+ */
+
+public class Matrizes1 {
+    public static void main(String[] args) {
+        //declaração e definição de tamanho
+        int numeros[][] = new int[3][3];
+
+        //declarar, definir o tamanho e inicializar
+        int mais_numeros [][] = { {1, 2, 3}, {4, 5, 6}, { 7, 8, 9}};
+
+    }
+}
