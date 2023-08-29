a942e61984fa5ac69563384b9609a7cb2107a7ab
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-08-29 18:38:21 -0300

    Matrizes parte 2

--- Matrizes1.java
+++ Matrizes1.java
@@ -26,10 +26,10 @@ numeros [2][2] = 32;
 public class Matrizes1 {
     public static void main(String[] args) {
         //declaração e definição de tamanho
-        int numeros[][] = new int[3][3];
+       // int numeros[][] = new int[3][3];
 
         //declarar, definir o tamanho e inicializar
-        int mais_numeros [][] = { {1, 2, 3}, {4, 5, 6}, { 7, 8, 9}};
+        //int mais_numeros [][] = { {1, 2, 3}, {4, 5, 6}, { 7, 8, 9}};
 
     }
 }
